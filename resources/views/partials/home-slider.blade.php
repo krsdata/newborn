d80a2120476fa9e1076f749d406d2e68a5d82a70
krsdata <krsdata.net@gmail.com>
2017-12-03 03:15:48 +0530

    changes

--- resources/views/partials/home-slider.blade.php
+++ resources/views/partials/home-slider.blade.php
@@ -1,75 +1,78 @@
+   
   <div class="banner-section">
     <div class="container">
       <div class="row">
         <div class="col-sm-8">
-          <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-            <ol class="carousel-indicators">
-                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-            </ol>
+            <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+                <ol class="carousel-indicators">
+                  <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                  <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                  <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                </ol>
 
-              <div class="carousel-inner" role="listbox">
-                <div class="item active">
-                    <img src="{{ asset('assets/img/toy1.png')}}" alt="...">
-                
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner" role="listbox">
+                    <div class="item active">
+                        <img src="{{ asset('assets/img/toy1.png')}}" alt="...">
+                    </div>
+                    <div class="item">
+                        <img src="{{ asset('assets/img/toy1.png')}}" alt="...">
+                    
+                    </div>
+                    <div class="item">
+                        <img src="{{ asset('assets/img/toy1.png')}}" alt="...">
+                     </div>
                 </div>
-                <div class="item">
-                    <img src="{{ asset('assets/img/toy1.png')}}" alt="...">
-                </div>
-              <div class="item">
-                    <img src="{{ asset('assets/img/toy1.png')}}" alt="...">
-                </div>
-              </div> 
-          </div>
-        </div> 
+            <!-- Controls -->
+            </div>
+        </div>
+                  
         <div class="col-sm-4 banner-add">
-          <div class="banner-heading">
-            <h1>Discover a monthly Baby Toy Boxes of lorem ipsum dolor sit amet</h1>
-              <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-              <!-- Indicators -->
-              <ol class="carousel-indicators">
-                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-              </ol>
+            <div class="banner-heading">
+                <h1>Discover a fun surprise in every Gift Box. A gift from US</h1>
+                    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                      <!-- Indicators -->
+                        <ol class="carousel-indicators">
+                          <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                          <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                          <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                       </ol>
 
-              <!-- Wrapper for slides -->
-              <div class="carousel-inner" role="listbox">
-                <div class="item active">
-                 <img src="{{ asset('assets/img/box1.png')}}" alt="...">
-                 <h4>Baby Toys Mini Box with 3 Items</h4>
-                 <p>Lorem ipsum dolor is the dummy text
-            for describing the web site content
-            as dummy its a mockup.</p>
-                  
+                        <!-- Wrapper for slides -->
+                      <div class="carousel-inner" role="listbox">
+                          <div class="item active">
+                             <img src="{{ asset('assets/img/box1.png')}}" alt="...">
+                           <h4>Build a Gift Box in 3 Easy Steps</h4>
+                           <p>Give back to those who matter most, and get a 30% limited time discount on ANY Box size.
+                          Explore our Gift Box Builder for free and customize your gifts in 3 easy steps.</p>
+                          <ul><b><li>1. Choose Box Size</li> <li>2. Choose Box Content Features</li> <li>3. Checkout Sent</li></b></ul>
+                            
+                            </div>
+                        <div class="item">
+                          <img src="{{ asset('assets/img/box1.png') }}" alt="...">
+                         <h4>Build a Gift Box in 3 Easy Steps</h4>
+                         <p>Give back to those who matter most, and get a 30% limited time discount on ANY Box size.
+                    Explore our Gift Box Builder for free and customize your gifts in 3 easy steps.</p>
+                    <ul><b><li>1. Choose Box Size</li> <li>2. Choose Box Content Features</li> <li>3. Checkout Sent</li></b></ul>
+                      </div>
+                    <div class="item">
+                       <img src="{{ asset('assets/img/box1.png')}}" alt="...">
+                       <h4>Build a Gift Box in 3 Easy Steps</h4>
+                       <p>Give back to those who matter most, and get a 30% limited time discount on ANY Box size.
+                  Explore our Gift Box Builder for free and customize your gifts in 3 easy steps.</p>
+                  <ul><b><li>1. Choose Box Size</li> <li>2. Choose Box Content Features</li> <li>3. Checkout Sent</li></b></ul>
+                      </div>
+             
                 </div>
-                <div class="item">
-                   
-                    <img src="{{ asset('assets/img/box1.png')}}" alt="...">
-                   <h4>Baby Toys small Box with 5 Items</h4>
-                 <p>Lorem ipsum dolor is the dummy text
-            for describing the web site content
-            as dummy its a mockup.</p>
-                </div>
-              <div class="item">
-                  
-                  <img src="{{ asset('assets/img/box1.png')}}" alt="...">
-                   <h4> Baby Toys large Box with 8 Items</h4>
-                 <p>Lorem ipsum dolor is the dummy text
-            for describing the web site content
-            as dummy its a mockup. </p>
-                </div>
-               
-              </div>
 
-              <!-- Controls -->
-              
-            </div>
+            <!-- Controls -->
+            
+          </div>
 
-            <div class="get-start">
-                <a href="#">Get Started</a>
-            </div>
+          <div class="get-start">
+            <a href="#">Get Started</a>
+          </div>
 
           </div>
         </div>
@@ -77,3 +80,6 @@
       </div>
     </div>
   </div>
+  <!--end banner-->
+  
+  
